45b14c73ee4917d4913957b3a33b2e2c4f39cce6
OscarW99 <54639882+OscarW99@users.noreply.github.com>
2020-09-29 09:52:25 +0100

    Update bioinformatics_web_app.py

--- bioinformatics_web_app.py
+++ bioinformatics_web_app.py
@@ -5,25 +5,74 @@ Created on Mon Sep 28 12:24:19 2020
 @author: oscar
 """
 
-""" A bioinformatics web app with streamlit and biopython """
 # a comment
 
 import streamlit as st
+from Bio import SeqIO
+from Bio.Seq import Seq
+
 
 def main():
     """bioinformatics_web_app"""
-    st.title('This is a title')
-    st.header('This is a header')
-    st.subheader('This is a subheader')
-    st.text('writing some text')
+    st.title('Sequence Analysis')
+    #st.header('This is a header')
+    #st.subheader('This is a subheader')
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    file = st.file_uploader("Upload FASTA file of DNA", type=["fasta", "txt"])
     
+    if st.button("submit"):
+        if file is not None:
+            radio = st.radio("Select option to view", ('RNA sequence','DNA sequence', 'Protein sequence'))
+            seq_record = SeqIO.read(file, "fasta")
+            sequence = str(seq_record.seq)
+            seq_ob = Seq(sequence)
+            transcribed = seq_ob.transcribe()
+            translated = seq_ob.translate()
+            if radio == 'DNA sequence':
+                st.write(seq_ob)
+            elif radio == 'RNA sequence':
+                st.write(transcribed)
+            elif radio == 'Protein sequence':
+                st.write(translated)
+                print(translated)
+        
+
+
+
+            
+            
+            
+            #else:
+                #st.error("Need to upload file")
+   
+            
+ 
+
+        
+        
+
+    
+# =============================================================================
+#     st.text('writing some text')
+#     if st.checkbox('check-me'):
+#         st.write('i am checked!')
+#     st.markdown("##### This is markdown 5")
+#     st.markdown("# This is markdown 1")
+#     st.success("success!")
+#     st.warning("Warning!")
+#     st.info("Information")
+#     st.error("Error!")
+#     radio = st.radio("Yes or No?", ('Yes', 'No'))
+#     if radio == 'Yes':
+#         st.write("you've selected yes")
+#     else:
+#         st.write("you've selected no")
+# =============================================================================
 if __name__ == '__main__':
     main()
     
 
     
     
-# in cmd:   streamlit run nameoffile.py
-# a random comment
-print('hello world')
-#another change
+# TO RUN THE FILE, in cmd typ:   streamlit run nameoffile.py
+

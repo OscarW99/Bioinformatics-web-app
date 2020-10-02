e48ffc69ecf059f1739f479f588fcab2db485054
OscarW99 <54639882+OscarW99@users.noreply.github.com>
2020-10-02 07:56:23 +0100

    Update bioinformatics_web_app.py

--- bioinformatics_web_app.py
+++ bioinformatics_web_app.py
@@ -18,6 +18,13 @@ import matplotlib.pyplot as plt
 
 
 
+aas = pd.read_csv('csv_of_aas.txt')
+full_names = [name for name in aas['Full_name']]
+abbreviation = [ab for ab in aas['Abbreviation']]
+aa_dict = dict(zip(abbreviation, full_names))
+aa_dict['*'] = 'Stop'
+
+
 #@st.cache(suppress_st_warning=True)
 def main():
     """bioinformatics_web_app"""
@@ -64,13 +71,61 @@ def main():
             plt.title('Nucleotide frequencies')
             st.pyplot()
         elif radio == 'RNA analysis':
-            st.write(transcribed)
+            st.write('RNA sequence:  ', transcribed)
+            lenght = str(len(transcribed))
+            section = '### Sequence length:  ' + "**" + str(len(transcribed)) + "**"
+            st.markdown(section)
+            st.write("")
+            G = [transcribed.count('G')]
+            A = [transcribed.count('A')]
+            U = [transcribed.count('U')]
+            C = [transcribed.count('C')]
+            GC_content = round((G[0]+C[0])/len(seq_ob)*100, 2)
+            AU_content = round((A[0]+U[0])/len(seq_ob)*100, 2)
+            st.write('GC content: ', GC_content, '%')
+            st.write('AU content: ', AU_content, '%')
+            rows = ['G', 'A', 'U', 'C']
+            df = pd.DataFrame(np.array([G,A,U,C]), columns = ['Count'], index = rows)
+            st.markdown("### Nucleotide count:")
+            st.dataframe(data=df, width=1000, height=1000)
+            #hide auto-error message with matplotlib
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            #plot bar graph of nucleotide frequencies
+            plt.bar(rows, [G[0],A[0],U[0],C[0]])
+            plt.title('Nucleotide frequencies')
+            st.pyplot()
         elif radio == 'Protein analysis':
-            st.write(translated)
-        
-   
-            
-            
+            st.write('Protein sequence: ', translated)
+            lenght = str(len(translated))
+            section = '### Sequence length:  ' + "**" + str(len(translated)) + "**"
+            st.markdown(section)
+            st.write("")
+            aa_count = {}
+            for letter in translated:
+                if letter in aa_count.keys():
+                    aa_count[letter] += 1
+                else:
+                     aa_count[letter] = 1
+            display_dict = {}
+            for key, value in aa_count.items():
+                display_dict[aa_dict[key]] = value
+            reverse_aa_dict = {value:key for key,value in aa_dict.items()}
+            display_names = [key for key in display_dict.keys()]
+            display_counts = [value for value in display_dict.values()] 
+            temp_zip = list(zip(display_names, display_counts))
+            temp_zip.sort(key= lambda x: x[1], reverse=True)
+            names = [tup[0] for tup in temp_zip]
+            names2 = [(name + "(" + str(reverse_aa_dict[name]) + ")") for name in names]
+            counts = [tup[1] for tup in temp_zip]
+            aa_df = pd.DataFrame(counts, columns = ['Count'], index = names2)
+            st.markdown("### Amino Acid count:")
+            st.dataframe(aa_df, width=1000, height=1000)
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            #plot bar graph of amino acid frequencies
+            ordered_abbreviations = [reverse_aa_dict[name] for name in names]
+            plt.bar(ordered_abbreviations, counts)
+            plt.title('Amio Acid frequencies')
+            st.pyplot()
 
    
             
